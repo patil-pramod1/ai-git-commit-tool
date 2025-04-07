8950300e8d2306124b8d80d74bdb038c242e9e5f
LOGITH J <logith@adaptnxt.com>
2025-04-07 18:21:21 +0530

    Add print statements to indicate the start and end of PR description generation in __main__.py

--- aicommit/src/__main__.py
+++ aicommit/src/__main__.py
@@ -2,4 +2,6 @@ from .aicommit import main
 from .pr_description_gen import generate_description
 if __name__ == "__main__":
     main()
+    print('Start generating PR description...')
     generate_description()
+    print('PR description generated successfully!')
